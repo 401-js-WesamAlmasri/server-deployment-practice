eee4e45e9948bfccee16567f470b8bc54be3a558
Wesam Al-Masri <mr0virus@gmail.com>
2021-05-16 20:08:14 +0300

    setup server

---
+++ __test__/server.test.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const server = require('../server');
+const supertest = require('supertest');
+const request = supertest(server.app);
+
+describe('server', () => {
+  it('should get 404 status', async () => {
+    const response = await request.get('/foo');
+    expect(response.status).toBe(404);
+  });
+
+  it('should get a welcome message', async () => {
+    const response = await request.get('/');
+    expect(response.status).toBe(200);
+    expect(response.text).toBe('welcome to server.js');
+  });
+
+  it('should get an error', async () => {
+    const response = await request.get('/bad');
+    expect(response.status).toEqual(500);
+  });
+});
---
+++ index.js
@@ -0,0 +1,5 @@
+const server = require('./server');
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+
+server.start(PORT);
